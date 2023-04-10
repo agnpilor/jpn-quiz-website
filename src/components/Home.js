a026fedc11ff2155f94c7752b776d5a5273dbd57
agnpilor <angel.pilor@itas.ca>
2023-04-10 12:51:31 -0700

    edited home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import homebanner from "../images/home-banner.png";
 import "../styles/Home.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
+import provision from "../images/provision.png"
+import textbook from "../images/text-book.png"
+import confetti from "../images/confetti.png"
 
 const Home = () => {
   return (
@@ -11,22 +13,47 @@ const Home = () => {
       <Navbar />
       <div className="home-banner">
         <div className="home-banner-left">
-          <img src={homebanner} alt="home banner" />
-        </div>
-        <div className="home-banner-right">
-          <h1>Learn Japanese now!</h1>
+          <h1>こんにちわ！</h1>
           <p>
-            Learn Japanese easily with our user-friendly website! Our lessons
-            are based on the popular Genki I textbook, covering everything from
-            basic grammar and vocabulary to advanced conversation skills. With
-            interactive exercises and quizzes, you'll have plenty of
-            opportunities to practice and improve your language abilities. Start
-            exploring the fascinating world of Japanese language and culture
-            today!
+          Discover the joy of learning Japanese with Kuizu! 
+          Our quizzes are designed to make language learning engaging and easy, with questions 
+          based on the Genki I textbook. Join us today and take your first step towards 
+          mastering this beautiful language!
           </p>
-        </div>
+          </div>
+          <div className="home-banner-right">
+            <img src={homebanner}/>
+          </div>
       </div>
-      <div className="browse-quizzes">{/* <QuizSlider /> */}</div>
+      <div className="kuizu-icons">
+        <div className="kuizu-icon">
+        <div className="kuizu-icon-bg">
+          <img src={confetti} alt="Fun Icon" />
+          </div>
+          <h3>Fun and Interactive</h3>
+          <p>Kuizu is designed to make learning Japanese engaging and enjoyable. 
+            With a range of quizzes that are tailored to different levels of proficiency, 
+            users can progress at their own pace and enjoy the process of learning.</p>
+        </div>
+        <div className="kuizu-icon">
+        <div className="kuizu-icon-bg">
+          <img src={textbook} alt="Textbook Icon" />
+          </div>
+          <h3>Based on the Genki I textbook</h3>
+          <p>Our quizzes are based on the Genki I textbook, which is one of the most popular 
+            textbooks for learning Japanese. By using quizzes that are aligned with this 
+            textbook, users can reinforce their knowledge of grammar, vocabulary, and kanji.</p>
+        </div>
+        <div className="kuizu-icon">
+        <div className="kuizu-icon-bg">
+          <img src={provision} alt="Flexible Icon" />
+          </div>
+          <h3>Flexible and Accesible</h3>
+          <p>Kuizu is accessible to users around the world and can be used on a range of 
+            devices. With a flexible format that allows users to access quizzes at any time, our 
+            website offers a convenient and effective way to learn Japanese at their own pace.</p>
+        </div>
+        </div>
       <Footer />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,40 @@
 import React from "react";
 import logo from "../images/logo.png";
 import "../styles/Navbar.css";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const activeStyle = ({isActive}) => {
+    return isActive ? "nav-active" : null
+  };
+
   return (
-    <div>
-      <nav className="navbar">
-        <div className="navbar-left">
-          <ul className="navbar-links">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/quizzes">Quizzes</a>
-            </li>
-            <li>
-              <a href="/about">About Us</a>
-            </li>
-            <li>
-              <a href="/resources">Resources</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-middle">
-          <img src={logo} alt="Kuizu Logo" />
-          {/* figure out why logo isnt centering when text/buttons is increased in size */}
-        </div>
-        {/* <div className="navbar-right">
-          <button className="navbar-button">Login</button>
-          <button className="navbar-button">Register</button>
-        </div> */}
-      </nav>
-    </div>
+<header className="navbar">
+  <div className="navbar-left">
+<Link to="/">
+        <img
+          className="logo"
+          src={logo}
+          alt="Kuizu logo"
+          title="Kuizu Logo | Home"
+        />
+      </Link>
+      </div>
+      <div className="navbar-right">
+      <NavLink to='/' activeStyle>
+            Home
+          </NavLink>
+          <NavLink to='/quizzes' activeStyle>
+            Quizzes
+          </NavLink>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/resources' activeStyle>
+            Resources
+          </NavLink>
+          </div>
+  </header>
   );
 };
 
